fc8fea4487c57b32b8cfaee44d78891fb8fbe617
Oscar Lagatta <oscarlagatta@gmail.com>
2019-11-26 22:11:33 +0000

    Refactor JSX to Stateless Functional Components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App(props) {
   return (
     <div className="App">      
       <div className="Todo-App">
-        <form>
-          <input type="text" />
-        </form>
-        <div className="Todo-List">
-          <ul>
-            {props.todos.map(todo => (
-              <li key={todo.id}>
-                <input type="checkbox" defaultChecked={todo.isComplete} />
-                {todo.name}
-              </li>
-            ) )}
-          </ul>
-        </div>
+        <TodoForm />
+        <TodoList todos={props.todos}/>
       </div>
     </div>
   ); 
 }
-
-// const App = ({todos}) => {
-//   return (
-//     <div className="App">      
-//       <div className="Todo-App">
-//         <form>
-//           <input type="text" />
-//         </form>
-//         <div className="Todo-List">
-//           <ul>
-//             {todos.map(todo => (
-//               <li key={todo.id}>
-//                 <input type="checkbox" defaultChecked={todo.isComplete} />
-//                 {todo.name}
-//               </li>
-//             ) )}
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-
 
 export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// () means implicit returns in multiple lines
+export default props => (
+  <form>
+    <input type="text" />
+  </form>
+);
