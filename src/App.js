999d5061d0ce9e69a97d1f3d6e2adc1a05d435ff
Oscar Lagatta <oscarlagatta@gmail.com>
2019-11-26 23:49:27 +0000

    react redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import React from "react";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App(props) {
   return (
-    <div className="App">      
+    <div className="App">
       <div className="Todo-App">
-        <TodoForm 
-        currentTodo={props.currentTodo} 
-        changeCurrent={props.changeCurrent}/>
-        <TodoList todos={props.todos}/>
+        <TodoForm
+          currentTodo={props.currentTodo}
+          changeCurrent={props.changeCurrent}
+        />
+        <TodoList todos={props.todos} />
       </div>
     </div>
-  ); 
+  );
 }
 
-export default App;
+// export default App;
+
+/**
+ * MapStateToProps is a function that's going to accept the entire state
+ * and return an object that represents the subset of the state we want
+ * to pass into our component.
+ * In this case, we want to pass the entire state, because it only consists
+ * of our todos array and our current todo string at this point.
+ */
+const mapStateToProps = state => state;
+/**
+ * connected app component will receive our state properties as props, 
+ * just like when we were passing them in manually, but now the Provider 
+ * and the Connect function will work together to make that happen.
+ */
+const ConnectedApp = connect(mapStateToProps)(App);
+export default ConnectedApp;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store";
-import {updateCurrent} from './reducers/todo';
+import { updateCurrent } from "./reducers/todo";
 
-const todoChangeHandler = (val) => store.dispatch(updateCurrent(val))
-
-const render = () => {
-  /**
-   * Here, we're using the store's getState method to get the global state
-   * out of our store and we're passing it into our app component as props.
-   * This happens once when the application loads.
-   */
-  const state = store.getState();
-
-  ReactDOM.render(
-    <App todos={state.todos} currentTodo={state.currentTodo} 
-      changeCurrent={todoChangeHandler}
-    />,
-    document.getElementById("root")
-  );
-};
-
-render();
+const todoChangeHandler = val => store.dispatch(updateCurrent(val));
 
 /**
- * Now anytime there's an update to the state object in the store, subscribe is going to call
- * the render function and update our application.
+ * The Provider component is going to take care of getting the state and 
+ * subscribing to state changes for us.
+ * Provider wraps app component. It's going to provide it with the store 
+ * and subscription, but in order to do that, we need to pass in the store prop 
+ * to Provider, and give it a reference to the store, which we're importing up 
+ * here as store.
  */
-store.subscribe(render);
+ReactDOM.render(
+  <Provider store={store}>
+    <App changeCurrent={todoChangeHandler} />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
