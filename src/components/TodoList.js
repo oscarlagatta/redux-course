c9a405c268f1a1dfd7b05df46e9169677c251622
Oscar Lagatta <oscarlagatta@gmail.com>
2019-11-28 00:04:12 +0000

    dispatch actions with redux thunk

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,31 @@
-import React from "react";
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchTodos } from "../reducers/todo";
 
-const TodoItem = ({id, name, isComplete}) => (
+const TodoItem = ({ id, name, isComplete }) => (
   <li key={id}>
     <input type="checkbox" defaultChecked={isComplete} />
     {name}
   </li>
 );
 
-const TodoList = props => (
-  <div className="Todo-List">
-    <ul>
-      {props.todos.map(todo => (
-        <TodoItem key={todo.id} {...todo} />
-      ))}
-    </ul>
-  </div>
-);
+class TodoList extends Component {
 
-export default connect(
-  state => ({todos: state.todos})
-)(TodoList);
+  componentDidMount(){
+    this.props.fetchTodos()
+  }
+  render() {
+    return (
+      <div className="Todo-List">
+        <ul>
+          {this.props.todos.map(todo => (
+            <TodoItem key={todo.id} {...todo} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default connect(state => ({ todos: state.todos }),
+{fetchTodos})(TodoList);
