c68f94310dde7b7263928a7978a0c4c2fd3582d6
Oscar Lagatta <Oscar.Lagatta@jato.com>
2019-11-29 13:53:08 +0000

    adds react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import Message from './components/Message';
+import Footer from './components/Footer';
 
 function App(props) {
   return (
     <div className="App">
-      <div className="Todo-App">
-        <Message />
-        <TodoForm />
-        <TodoList />
-      </div>
+      <Router>
+        <div className="Todo-App">
+          <Message />
+          <TodoForm />
+          <TodoList />
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 }
