d5026f5dab0b86ae69186eb8f0da29afe9f671be
Oscar Lagatta <Oscar.Lagatta@jato.com>
2019-11-29 09:39:32 +0000

    creates display message component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import "./App.css";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
+import React from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import Message from './components/Message';
 
 function App(props) {
   return (
     <div className="App">
       <div className="Todo-App">
+        <Message />
         <TodoForm />
         <TodoList />
       </div>
